ae2f390dd53a5574588b90a99d8149350b7922d2
Newsstate <newsstate6294@gmail.com>
2025-07-24 17:57:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from routes.audit import audit_blueprint
 
-app = Flask(__name__, static_folder="../frontend/static", template_folder="../frontend/templates")
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
 app.register_blueprint(audit_blueprint)
 
 @app.route("/")
